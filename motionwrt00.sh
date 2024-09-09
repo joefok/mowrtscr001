366ca6f9374cf70d6c4ce6f97ec1951cf1f8188e
joefok <84949844+joefok@users.noreply.github.com>
2024-09-09 14:45:24 +0800

    Update motionwrt00.sh

--- motionwrt00.sh
+++ motionwrt00.sh
@@ -1,2 +1,6 @@
 #!/bin/ash
 echo HelloWorld;
+# attempt to ssh session from console
+# sshpass -f /root/ubpwd ssh rootsu@$(cat /root/ubhost) 
+# sshpass -f /root/ubpwd autossh -M 41381 rootsu@$(cat /root/ubhost)
+# sshpass -f /root/ubpwd autossh -M 41381 -o StrictHostKeyChecking=no rootsu@$(cat /root/ubhost)
