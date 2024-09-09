ec3b48d984992bb61b3c39843cfc497677483929
joefok <84949844+joefok@users.noreply.github.com>
2024-09-09 15:34:24 +0800

    Update motionwrt00.sh

--- motionwrt00.sh
+++ motionwrt00.sh
@@ -1,9 +1,10 @@
 #!/bin/ash
 echo HelloWorld;
 # attempt to ssh session from console
-# sshpass -f /root/ubpwd ssh rootsu@$(cat /root/ubhost) 
+# SSH CONSOLE MANUALLY ->    sshpass -f /root/ubpwd ssh -y rootsu@$(cat /root/ubhost) 
 # sshpass -f /root/ubpwd autossh -M 41381 rootsu@$(cat /root/ubhost)
 # sshpass -f /root/ubpwd autossh -M 41381 -o StrictHostKeyChecking=no rootsu@$(cat /root/ubhost)
 # rm /root/.ssh/known_hosts
 # rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y rootsu@$(cat /root/ubhost);
+
 rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y -N -R 39285:127.0.0.1:80 rootsu@$(cat /root/ubhost)&
