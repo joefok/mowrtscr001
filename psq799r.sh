1ec3ab685e3fa29662bd6db011a823f238dac8f8
joefok <84949844+joefok@users.noreply.github.com>
2024-09-25 14:00:50 +0800

    Update psq799r.sh

--- psq799r.sh
+++ psq799r.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+#  sshpass -f /root/ubpwd ssh -y rootsu@$(cat /root/ubhost) -p 60922;
+# CONNECT TO SSH ->>>>>>       sshpass -p $(cat /root/ubpwd) ssh rootsu@$(cat /root/ubhost) -p 60922
+
+
+
