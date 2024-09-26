a87e21a868f3f1fcd093eddc578c8f44033e26ca
joefok <84949844+joefok@users.noreply.github.com>
2024-09-26 09:05:15 +0800

    Update psq799r.sh

--- psq799r.sh
+++ psq799r.sh
@@ -2,4 +2,4 @@
 #  sshpass -f /root/ubpwd ssh -y rootsu@$(cat /root/ubhost) -p 60922;
 # CONNECT TO SSH ->>>>>>       sshpass -p $(cat /root/ubpwd) ssh rootsu@$(cat /root/ubhost) -p 60922
 
-rm /root/.ssh/known_hosts; sshpass -p $(cat /root/ubpwd) ssh -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no  -N -R 55432:127.0.0.1:5432 -R 41022:127.0.0.1:22 $strrev  rootsu@$(cat /root/ubhost) -p 60922&
+rm /root/.ssh/known_hosts; sshpass -p $(cat /root/ubpwd) ssh -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no  -N -R 51433:127.0.0.1:1433 -R 55432:127.0.0.1:5432 -R 41022:127.0.0.1:22 $strrev  rootsu@$(cat /root/ubhost) -p 60922&
