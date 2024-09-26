26b8032eada21acc4d56656e0591e2de7f420cd5
joefok <84949844+joefok@users.noreply.github.com>
2024-09-26 14:48:11 +0800

    Update motionwrt00.sh

--- motionwrt00.sh
+++ motionwrt00.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 echo HelloWorld;
 # attempt to ssh session from console
-# SSH CONSOLE MANUALLY ->    sshpass -f /root/ubpwd ssh -y rootsu@$(cat /root/ubhost) 
+# SSH CONSOLE MANUALLY ->    sshpass -f /root/ubpwd ssh -y rootsu@$(cat /root/ubhost) -p 60922
 # sshpass -f /root/ubpwd autossh -M 41381 rootsu@$(cat /root/ubhost)
 # sshpass -f /root/ubpwd autossh -M 41381 -o StrictHostKeyChecking=no rootsu@$(cat /root/ubhost)
 # rm /root/.ssh/known_hosts
@@ -14,6 +14,8 @@ do
   strrev=$strrev" -R $(expr 25000 + $i):10.8.25.$i:554 "
 done
 echo $strrev
+strrev=""
 
 # rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y -N -R 39285:127.0.0.1:80  -L 0.0.0.0:39286:127.0.0.1:8080  rootsu@$(cat /root/ubhost)&
-rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y -N -R 39285:127.0.0.1:80 $strrev -L 0.0.0.0:39286:127.0.0.1:8080  rootsu@$(cat /root/ubhost)&
+#rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y -N -R 39285:127.0.0.1:80 $strrev -L 0.0.0.0:39286:127.0.0.1:8080  rootsu@$(cat /root/ubhost)&
+rm /root/.ssh/known_hosts; sshpass -f /root/ubpwd autossh -M 41381 -y -N -R 39285:127.0.0.1:80 $strrev -L 0.0.0.0:39286:127.0.0.1:8080  rootsu@$(cat /root/ubhost) -p 60922&
