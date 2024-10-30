035066deecc0e614521a67e42e8dd88fc269f62c
joefok <84949844+joefok@users.noreply.github.com>
2024-10-30 18:53:25 +0800

    Update xiaomiowrt.sh

--- xiaomiowrt.sh
+++ xiaomiowrt.sh
@@ -8,7 +8,7 @@ wget   http://127.0.0.1:8002/html/index.html?noredirect  -O /tmp/index_ten.html 
 port=$(tr -cd 0-9 </dev/urandom | head -c 3);
 
 if [[ ! -f /tmp/index_azub.html ]]; then
-sshpass -f /root/passwdazub.txt autossh -M 22$port -y -y -o "ExitOnForwardFailure yes" -N -L 0.0.0.0:25541:127.0.0.1:38282 -R 25581:192.168.8.103:80   -R 25582:192.168.8.106:80  -R 25583:192.168.8.176:80  -R 25541:192.168.8.103:554  -R 25542:192.168.8.106:554 -R 25543:192.168.8.176:554 -L 58000:127.0.0.1:58001 -R 58001:192.168.8.1:80 -R 51681:192.168.8.100:11681 -R 41681:192.168.8.114:11681 rootsu@$(cat /root/azubhost) -p 60922&
+sshpass -f /root/passwdazub.txt autossh -M 22$port -y -y -o "ExitOnForwardFailure yes" -N -L 0.0.0.0:25541:127.0.0.1:38282 -R 25581:192.168.8.103:80   -R 25582:192.168.8.106:80  -R 25583:192.168.8.176:80  -R 25541:192.168.8.103:554  -R 25542:192.168.8.106:554 -R 25543:192.168.8.176:554 -L 58000:127.0.0.1:58001 -R 58001:192.168.8.1:80 -R 51681:192.168.8.100:11681 -R 41681:192.168.8.114:11681 -R 127.0.0.1:30023:127.0.0.1:22 rootsu@$(cat /root/azubhost) -p 60922&
 fi;
 
 port=$(tr -cd 0-9 </dev/urandom | head -c 3);
