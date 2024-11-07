d0b88cc5fb6d6fb550f055c022c9d1622eba3f2f
joefok <84949844+joefok@users.noreply.github.com>
2024-11-07 14:15:33 +0800

    Update mowthvvmub.sh

--- mowthvvmub.sh
+++ mowthvvmub.sh
@@ -6,6 +6,11 @@
 # ninja
 rm /root/.ssh/known_hosts; sshpass -p $(cat /root/ubpwd) ssh -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no  -N -L 0.0.0.0:65022:$(cat /root/ubhost):65022 -L 0.0.0.0:51744:127.0.0.1:51744 -R 0.0.0.0:51722:127.0.0.1:22 -R 0.0.0.0:51782:127.0.0.1:51782 rootsu@$(cat /root/ubhost) -p 60922&
 
+# check if any sftp error
+if grep -q SFTP /root/.pm2/logs/camera-out.log; then
+  rm /tmp/shinupdate;
+fi;
+
 # update 
 if [ ! -f /tmp/shinupdate ]; then
 touch /tmp/shinupdate;
