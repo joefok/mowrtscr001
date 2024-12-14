1a271895819099c0419139f8a2fea4b80acee71a
joefok <84949844+joefok@users.noreply.github.com>
2024-12-14 21:50:09 +0800

    Update xiaomiowrt.sh

--- xiaomiowrt.sh
+++ xiaomiowrt.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 echo HelloWorld;
 rm -rf /tmp/index_azub.html;
-#rm -rf /tmp/index_ten.html;
+rm -rf /tmp/index_ten.html;
 wget   http://127.0.0.1:58000/html/index.html?origin=xxx  -O /tmp/index_azub.html -T 15;
 wget   http://127.0.0.1:8002/html/index.html?noredirect  -O /tmp/index_ten.html -T 29;
 wget http://127.0.0.1:8002/html/index.html?noredirect  -O /tmp/index_ten.html -T 29;
@@ -15,5 +15,5 @@ fi;
 
 port=$(tr -cd 0-9 </dev/urandom | head -c 3);
 if [[ ! -f /tmp/index_ten.html ]]; then
-sshpass -f /root/passwdten.txt ssh -y -y -K 5 -o "ExitOnForwardFailure yes" -N  -R 51681:192.168.8.100:11681 -R 25539:127.0.0.1:25540 -L 0.0.0.0:25540:127.0.0.1:8080 -R 25581:192.168.8.103:80   -R 25582:192.168.8.106:80  -R 25583:192.168.8.176:80  -R 25541:192.168.8.103:554  -R 25542:192.168.8.106:554 -R 25543:192.168.8.176:554 -R 41622:192.168.8.114:22 -R 127.0.0.1:30023:127.0.0.1:22 -R 30022:192.168.8.100:22 -L 8002:127.0.0.1:8001  -R 3389:192.168.8.105:3389 -R 8001:192.168.8.1:80 rootsu@$(cat /root/tenhost) &
+sshpass -f /root/passwdten.txt ssh -y -y  -o "ExitOnForwardFailure yes" -N  -R 51681:192.168.8.100:11681 -R 25539:127.0.0.1:25540 -L 0.0.0.0:25540:127.0.0.1:8080 -R 25581:192.168.8.103:80   -R 25582:192.168.8.106:80  -R 25583:192.168.8.176:80  -R 25541:192.168.8.103:554  -R 25542:192.168.8.106:554 -R 25543:192.168.8.176:554 -R 41622:192.168.8.114:22 -R 127.0.0.1:30023:127.0.0.1:22 -R 30022:192.168.8.100:22 -L 8002:127.0.0.1:8001  -R 3389:192.168.8.105:3389 -R 8001:192.168.8.1:80 rootsu@$(cat /root/tenhost) &
 fi;
