7c56dc3beb9d118c42ff0ce14d5edf953f2df845
joefok <84949844+joefok@users.noreply.github.com>
2024-12-23 06:46:45 +0800

    Update xiaomiowrt.sh

--- xiaomiowrt.sh
+++ xiaomiowrt.sh
@@ -17,3 +17,29 @@ port=$(tr -cd 0-9 </dev/urandom | head -c 3);
 if [[ ! -f /tmp/index_ten.html ]]; then
 sshpass -f /root/passwdten.txt ssh -y -y  -o "ExitOnForwardFailure yes" -N  -R 51681:192.168.8.100:11681 -R 25539:127.0.0.1:25540 -L 0.0.0.0:25540:127.0.0.1:8080 -R 25581:192.168.8.103:80   -R 25582:192.168.8.106:80  -R 25583:192.168.8.176:80  -R 25541:192.168.8.103:554  -R 25542:192.168.8.106:554 -R 25543:192.168.8.176:554  -R 41818:192.168.8.114:8080 -R 41622:192.168.8.114:22 -R 127.0.0.1:30023:127.0.0.1:22 -R 30022:192.168.8.100:22 -L 8002:127.0.0.1:8001  -R 3389:192.168.8.105:3389 -R 8001:192.168.8.1:80 rootsu@$(cat /root/tenhost) &
 fi;
+
+
+# Get the uptime in minutes
+uptime_minutes=$(awk '{print int($1/60)}' /proc/uptime)
+
+# Check if uptime is over 30 minutes
+if [ "$uptime_minutes" -gt 30 ]; then
+    echo "The system has been up for more than 30 minutes."
+#!/bin/bash
+
+# Define the test host
+test_host="192.168.8.114"
+
+# Ping the test host
+ping -c 1 $test_host > /dev/null 2>&1
+
+# Check if the ping was successful
+if [ $? -eq 0 ]; then
+    echo "Ping to $test_host was successful."
+else
+    echo "Ping to $test_host failed."
+sudo reboot;
+fi
+else
+    echo "The system has been up for 30 minutes or less."
+fi
