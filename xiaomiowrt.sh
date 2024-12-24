69b727a7993f89884003e55de9526378d7255831
joefok <84949844+joefok@users.noreply.github.com>
2024-12-24 19:20:01 +0800

    Update xiaomiowrt.sh

--- xiaomiowrt.sh
+++ xiaomiowrt.sh
@@ -23,7 +23,7 @@ fi;
 uptime_minutes=$(awk '{print int($1/60)}' /proc/uptime)
 
 # Check if uptime is over 30 minutes
-if [ "$uptime_minutes" -gt 15 ]; then
+if [ "$uptime_minutes" -gt 30 ]; then
     echo "The system has been up for more than 30 minutes."
 
 # Define the test host
