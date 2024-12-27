9c04d753b6d132f8fa4e1309c7b973707dc6efc3
joefok <84949844+joefok@users.noreply.github.com>
2024-12-27 09:48:00 +0800

    Update opionemain.sh

--- opionemain.sh
+++ opionemain.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
+# check if any discord error
+if grep -q discord /root/.pm2/logs/camera-error.log; then
+  rm /tmp/shinupdate;
+fi;
+
+# Over 3MB size
+if [ -n "$(find /root/.pm2/logs/camera-error.log -prune -size +3000000c)" ]; then
+pm2 flush;
+fi;
+
 # Get the uptime in minutes
 uptime_minutes=$(awk '{print int($1/60)}' /proc/uptime)
-
 # Check if uptime is over 30 minutes
 if [ "$uptime_minutes" -gt 30 ]; then
     echo "The system has been up for more than 30 minutes."
-#bash /tmp/opionemain.sh;
-
 else
     echo "The system has been up for 30 minutes or less."
+    touch /tmp/shinupdate;
 fi
 
+# update 
+if [ ! -f /tmp/shinupdate ]; then
+touch /tmp/shinupdate;
+sudo apt remove unattended-upgrades -fy;
+sed -i '1s/.*/APT::Periodic::Update-Package-Lists "0";/' /etc/apt/apt.conf.d/20auto-upgrades;
+sed -i '2s/.*/APT::Periodic::Unattended-Upgrade "0";/' /etc/apt/apt.conf.d/20auto-upgrades;
+sed -i '2s/.*/   "debugLog": true,/' /home/Shinobi/conf.json;
+cd /home/Shinobi;
+sh UPDATE.sh;
+cd /home/Shinobi;
+pm2 flush;
+cd /home/Shinobi;
+pm2 restart camera;
+cd /home/Shinobi;
+pm2 restart cron;
+killall ssh;
 
+NTP_SERVER="ntp.hkobservatory.gov.hk"
+sudo ntpdate $NTP_SERVER
 
+fi;
 
 
