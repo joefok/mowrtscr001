99543fc677a48afa4903785163f0f2e8723f9c61
joefok <84949844+joefok@users.noreply.github.com>
2024-12-28 10:44:10 +0800

    Update opionemain.sh

--- opionemain.sh
+++ opionemain.sh
@@ -76,7 +76,7 @@ fi;
 # Get the uptime in minutes
 uptime_minutes=$(awk '{print int($1/60)}' /proc/uptime)
 # Check if uptime is over 30 minutes
-if [ "$uptime_minutes" -gt 20 ]; then
+if [ "$uptime_minutes" -gt 30 ]; then
     echo "The system has been up for more than 30 minutes."
 else
     echo "The system has been up for 30 minutes or less."
