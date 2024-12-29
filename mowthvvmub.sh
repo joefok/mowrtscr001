84385ebe32acf59dc82392ae84adc84c715bb441
joefok <84949844+joefok@users.noreply.github.com>
2024-12-29 11:18:07 +0800

    Update mowthvvmub.sh

--- mowthvvmub.sh
+++ mowthvvmub.sh
@@ -49,3 +49,77 @@ cd /home/Shinobi;
 pm2 restart cron;
 killall ssh;
 fi;
+
+### MIGRATE RESTORE BACKUP ###
+
+# mysqldump and mysql password less
+FILE="/tmp/mysqldump.cnf "
+if [ -e "$FILE" ]; then
+    echo "The file exists."
+else
+    echo "The file does not exist."
+touch /tmp/mysqldump.cnf 
+chmod 600 /tmp/mysqldump.cnf
+echo [mysqldump] > /tmp/mysqldump.cnf 
+echo user=root >> /tmp/mysqldump.cnf 
+echo password=your_password >> /tmp/mysqldump.cnf 
+echo [mysql] >> /tmp/mysqldump.cnf 
+echo user=root >> /tmp/mysqldump.cnf 
+echo password=your_password >> /tmp/mysqldump.cnf 
+cat /tmp/mysqldump.cnf 
+fi
+
+# create backup for migration with password encrypted
+# Define the filename
+filename="/root/passwdmigrate"
+# Check if the file does not exist
+if [ ! -f "$filename" ]; then
+    echo "File does not exist."
+touch /root/passwdmigrate # nano edit this file
+echo 1234 > /root/passwdmigrate;
+cat /root/ubpwd > /root/passwdmigrate;
+else
+    echo "File exists."
+fi
+
+
+# restore migration
+FILE="/tmp/MOVMmigratearchive.tar.gz.gpg"
+if [ -e "$FILE" ]; then
+    echo "The file exists."
+else
+    echo "The file does not exist."
+wget -O /tmp/MOVMmigratearchive.tar.gz.gpg https://github.com/joefok/mowrtscr001/raw/refs/heads/main/MOVMmigratearchive.tar.gz.gpg
+gpg --batch --passphrase-file /root/passwdmigrate --output /tmp/migratearchive.tar.gz --decrypt /tmp/MOVMmigratearchive.tar.gz.gpg 
+fi
+
+DIR="/home/Shinobi"
+if [ -d "$DIR" ]; then
+    echo "The directory exists."
+FILE="/root/migratedataflag"
+if [ ! -f "$FILE" ]; then
+touch /root/migratedataflag
+cd /
+tar xzvf /tmp/migratearchive.tar.gz 
+mysql ccio < /tmp/ShinobiDatabaseBackup.sql
+cd /home/Shinobi
+npm install discord.js
+node tools/modifyConfiguration.js addToConfig='{"discordBot":true}'
+pm2 restart all
+fi
+else
+    echo "The directory does not exist."
+fi
+
+# backup migration
+FILE="/tmp/migratearchive.tar.gz"
+if [ -e "$FILE" ]; then
+    echo "The file exists."
+else
+    echo "The file does not exist."
+mysqldump --defaults-file=/tmp/mysqldump.cnf ccio > /tmp/ShinobiDatabaseBackup.sql
+tar czvf /tmp/migratearchive.tar.gz /tmp/ShinobiDatabaseBackup.sql /home/Shinobi/conf.json /home/Shinobi/super.json
+gpg --batch --passphrase-file /root/passwdmigrate --symmetric --cipher-algo aes256 -o /tmp/MOVMmigratearchive.tar.gz.gpg /tmp/migratearchive.tar.gz
+fi
+# scp /tmp/MOVMmigratearchive.tar.gz.gpg rootsu@localhost:/tmp/MOVMmigratearchive.tar.gz.gpg
+
